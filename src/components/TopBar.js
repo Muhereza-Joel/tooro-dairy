5e0bf45d7e218ca22a9b43ee4ecd3941677cba8b
Muhereza Joel <muherezajoel40@gmail.com>
2024-02-14 15:02:34 +0300

    added profile photo and username in UI

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { Dropdown, FlexboxGrid } from "rsuite";
+import Avator from "../assets/images/avator.jpg";
+import Cookies from "js-cookie";
 
 const TopBar = (props) => {
+  const [avatarUrl, setAvatarUrl] = useState("");
   const navigate = useNavigate();
+  const userId = Cookies.get("tdmis")
+    ? JSON.parse(Cookies.get("tdmis")).id
+    : null;
+  const username = Cookies.get("tdmis")
+    ? JSON.parse(Cookies.get("tdmis")).username
+    : null;
+
+  const avatorStyle = {
+    width: "150px",
+    height: "150px",
+    objectFit: "cover",
+    border: "2px solid #299ea6",
+    marginTop: "1rem",
+    marginBottom: "1rem",
+    backgroundColor: "#fff",
+    borderRadius: "50%",
+  };
+
+  useEffect(() => {
+    const fetchAvatarUrl = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3002/tdmis/api/v1/auth/get-avator/${userId}`
+        );
+
+        if (response.ok) {
+          const avatarData = await response.json();
+          if (Array.isArray(avatarData) && avatarData.length > 0) {
+            setAvatarUrl(avatarData[0].url);
+          } else {
+            console.error("Invalid avatar data structure");
+          }
+        } else {
+          console.error("Failed to fetch avatarUrl");
+        }
+      } catch (error) {
+        console.error("Error during fetch:", error);
+      }
+    };
+
+    fetchAvatarUrl();
+  }, []);
+
   return (
     <div>
       <div className="show-grid">
@@ -16,18 +62,38 @@ const TopBar = (props) => {
             colspan={12}
             style={{ textAlign: "end", fontSize: "1.3rem" }}
           >
-            <Dropdown title="Hello Miria" size="md">
-              <Dropdown.Item panel style={{ padding: 10, width: 160 }}>
+            <Dropdown title={`Hello ${username}`} size="md">
+              <Dropdown.Item
+                panel
+                style={{ padding: 10, width: 220, textAlign: "center" }}
+              >
+                <img
+                  src={avatarUrl || Avator}
+                  style={avatorStyle}
+                  alt="avatar"
+                />
                 <p>Signed in as</p>
-                <strong>foobar</strong>
+                <strong>{username}</strong>
               </Dropdown.Item>
               <Dropdown.Separator />
-              <Dropdown.Item onClick={() => {navigate('/dashboard/users/my-profile')}}>My profile</Dropdown.Item>
-              
+              <Dropdown.Item
+                onClick={() => {
+                  navigate("/dashboard/users/my-profile");
+                }}
+              >
+                My profile
+              </Dropdown.Item>
+
               <Dropdown.Separator />
               <Dropdown.Item>Help</Dropdown.Item>
               <Dropdown.Item>Settings</Dropdown.Item>
-              <Dropdown.Item onClick={() => {navigate('/auth/login')}}>Sign out</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  navigate("/auth/login");
+                }}
+              >
+                Sign out
+              </Dropdown.Item>
             </Dropdown>
           </FlexboxGrid.Item>
         </FlexboxGrid>
